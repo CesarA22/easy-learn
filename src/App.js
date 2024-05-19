c1839dd079824c48cb740c8339a520844b31f563
A-Wroblewski <62357109+A-Wroblewski@users.noreply.github.com>
2024-05-18 21:12:54 -0300

    trocando switch por routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 // src/App.js
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Login from './src/pages/login';
 import CreateAccount from './src/pages/create-account';
 import DashboardUser from './src/pages/dashboard-user';
 import UserScreen from './src/pages/user-screen';
 import './src/styles/login.css';
 import './src/styles/create-account.css';
-import '/src/styles/dashboard-user.css';
+import './src/styles/dashboard-user.css';
 import './src/styles/user-screen.css';
 
 function App() {
   return (
     <div className="App">
-      <Switch>
+      <Routes>
         <Route path="/" component={Login} />
         {/* Coloquei a rota raiz para o login, mas pode ser trocado depois */}
         <Route path="/create-account" component={CreateAccount} />
         <Route path="/dashboard-user" component={DashboardUser} />
         <Route path="/user-screen" component={UserScreen} />
-      </Switch>
+      </Routes>
     </div>
   );
 }
