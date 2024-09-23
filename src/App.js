f604d9a685a755473e107071c8ff84533c4acd62
Carlos <carlos.eduardo.fontes.c@gmail.com>
2024-09-23 16:52:34 -0300

    Arrumando páginas...

--- src/App.js
+++ src/App.js
@@ -10,6 +10,13 @@ import UserScreen from "./pages/user-screen.js";
 import CreateProduct from "./pages/create-product.js";
 import ProductDetails from './pages/product-details.js';
 
+import CreateAccountComprador from "./pages/create-account-comprador.js";
+import ProdutosDisponiveis from "./pages/produtos-disponiveis.js";
+import ProdutosComprados from "./pages/produtos-comprados.js";
+import LoginComprador from "./pages/login-comprador.js";
+import ConsumirProduto from "./pages/consumir-produto.js";
+import UserScreenComprador from "./pages/user-screen-comprador.js";
+
 import "./styles/landing-page.css";
 import "./styles/create-account.css";
 import "./styles/dashboard-user.css";
@@ -19,6 +26,14 @@ import "./styles/user-screen.css";
 import "./index.css";
 import "./styles/create-product.css";
 import "./styles/product-details.css";
+
+import "./styles/create-account-comprador.css";
+import "./styles/produtos-disponiveis.css";
+import "./styles/login-comprador.css";
+import "./styles/consumir-produto.css";
+import "./styles/produtos-comprados.css";
+import "./styles/user-screen-comprador.css";
+
 
 function App() {
   return (
@@ -32,9 +47,18 @@ function App() {
         <Route path="/products" element={<Products />} />
         <Route path="/user-screen" element={<UserScreen />} />
         <Route path="/login" element={<Login />} />
+
+        <Route path="/create-account-comprador" element={<CreateAccountComprador />} />
+        <Route path="/produtos-disponiveis" element={<ProdutosDisponiveis />} />
+        <Route path="/login-comprador" element={<LoginComprador />} />
+        <Route path="/consumir-produto" element={<ConsumirProduto />} />
+        <Route path="/produtos-comprados" element={<ProdutosComprados />} />
+        <Route path="/user-screen-comprador" element={<UserScreenComprador />} />
       </Routes>
     </div>
   );
 }
 
 export default App;
+
+// create-account / create-product / 
