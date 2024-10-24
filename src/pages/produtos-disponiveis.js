0f5c0c360852a99cec0e82d4c97a31a3eab70fb3
Carlos <carlos.eduardo.fontes.c@gmail.com>
2024-10-24 18:34:58 -0300

    Arrumando tela de produtos para comprador

--- src/pages/produtos-disponiveis.js
+++ src/pages/produtos-disponiveis.js
@@ -1,31 +1,53 @@
 // src/components/ProdutosDisponiveis.js
-import React, { useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
-import '../styles/produtos-disponiveis.css';
-import { AuthContext } from '../context/AuthContext';
+import React, { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import "../styles/produtos-disponiveis.css";
+import { AuthContext } from "../context/AuthContext";
 
-function ProdutosDisponiveis () {
+function ProdutosDisponiveis() {
   const navigate = useNavigate();
   const { user } = useContext(AuthContext);
+
+  const [products, setProducts] = useState([
+    { name: "prod-1", description: "Descrição do produto" },
+    { name: "prod-2", description: "Descrição do produto" },
+    { name: "prod-3", description: "Descrição do produto" },
+    { name: "prod-4", description: "Descrição do produto" },
+    { name: "prod-5", description: "Descrição do produto" },
+  ]);
+
+  // useEffect(() => {
+  // TODO: Fazer uma chamada para API
+  // e setar os produtos com setProducts
+  // setProducts()
+  // }, [])
 
   return (
     <div className="produtos-disponiveis-container">
       <aside className="sidebar">
-        <div className="logo" onClick={() => navigate('/')}>Easy Learn</div>
+        <div className="logo" onClick={() => navigate("/")}>
+          Easy Learn
+        </div>
         <nav className="menu">
           <ul>
-            <li onClick={() => navigate('/produtos-disponiveis')}>Produtos disponiveis</li>
-            <li onClick={() => navigate('/products-comprados')}>Produtos Compados</li>
+            <li onClick={() => navigate("/produtos-disponiveis")}>
+              Produtos disponiveis
+            </li>
+            <li onClick={() => navigate("/products-comprados")}>
+              Produtos Compados
+            </li>
           </ul>
         </nav>
         <div className="settings">
           <p>Configurações</p>
-          <p onClick={() => navigate('/user-screen-comprador')}>{user ? user.name : 'Nome do perfil'}</p>
+          <p onClick={() => navigate("/user-screen-comprador")}>
+            {user ? user.name : "Nome do perfil"}
+          </p>
         </div>
       </aside>
       <main className="main-content">
         <header>
-          <h1>Boas-vindas, {user ? user.name : 'Nome do perfil'}</h1>
+          <h1>Boas-vindas, {user ? user.name : "Nome do perfil"}</h1>
         </header>
 
         <section className="summary">
@@ -41,12 +63,22 @@ function ProdutosDisponiveis () {
             <span>R$ 0.00 - R$ 10.00</span>
           </div>
         </section>
-        <section className="product">
-          <h2>Produto exemplo 1</h2>
-          <div className="product-content"></div>
-        </section>
-        
-
+        <div className="products__container">
+          {products.map((product) => (
+            <section className="product">
+              <h2>Produto exemplo 1</h2>
+              <div className="product-content">
+                <div className="product-item" key={product.name}>
+                  <div className="product-image" />
+                  <div>
+                    <p>{product.name}</p>
+                    <p>{product.description}</p>
+                  </div>
+                </div>
+              </div>
+            </section>
+          ))}
+        </div>
       </main>
     </div>
   );
